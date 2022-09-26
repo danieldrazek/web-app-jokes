f0284ebabd9bf5bca98a49fa4b135a061fb0ffcd
Daniel Drazek <daniel_2000@o2.pl>
2022-09-26 19:31:33 +0100

    init commit

---
+++ src/main/java/pl/pp/spring/jokeswebapp/JokesWebAppApplication.java
@@ -0,0 +1,13 @@
+package pl.pp.spring.jokeswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokesWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pp/spring/jokeswebapp/JokesWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pp.spring.jokeswebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
