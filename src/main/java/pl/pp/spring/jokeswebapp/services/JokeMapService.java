baf64dbf3ae2f54d9291cae5c3a8a6f760ca9b4b
Daniel Drazek <daniel_2000@o2.pl>
2022-10-01 16:34:35 +0100

    Create example jokes. closes #4

--- src/main/java/pl/pp/spring/jokeswebapp/services/JokeMapService.java
+++ src/main/java/pl/pp/spring/jokeswebapp/services/JokeMapService.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 @Service
 public class JokeMapService implements JokeService{
-    public Map<Long, Joke> jokeMap = new HashMap<>();
+    private Map<Long, Joke> jokeMap = new HashMap<>();
 
     @Override
     public List<Joke> findAll() {
