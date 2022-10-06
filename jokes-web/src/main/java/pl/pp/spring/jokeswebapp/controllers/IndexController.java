39cf29a2fd31a15b1c997812d48477c01427c79e
Daniel Drazek <daniel_2000@o2.pl>
2022-10-06 22:47:26 +0100

    Show list category on index page. closes #12

--- jokes-web/src/main/java/pl/pp/spring/jokeswebapp/controllers/IndexController.java
+++ jokes-web/src/main/java/pl/pp/spring/jokeswebapp/controllers/IndexController.java
@@ -3,19 +3,23 @@ package pl.pp.spring.jokeswebapp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.pp.spring.jokeswebapp.services.CategoryService;
 import pl.pp.spring.jokeswebapp.services.JokeService;
 
 @Controller
 public class IndexController {
     private final JokeService jokeService;
+    private final CategoryService categoryService;
 
-    public IndexController(JokeService jokeService) {
+    public IndexController(JokeService jokeService, CategoryService categoryService) {
         this.jokeService = jokeService;
+        this.categoryService = categoryService;
     }
 
     @RequestMapping({"", "/", "/index"})
     public String showIndex(Model model) {
         model.addAttribute("jokes", jokeService.findAll());
+        model.addAttribute("categories", categoryService.findAll());
         return "index";
     }
 }
