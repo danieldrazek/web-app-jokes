9acc2b0b841d241e6dcf5cad48e9ab9f599f829b
Daniel Drazek <daniel_2000@o2.pl>
2022-10-12 21:51:54 +0100

    H2 database and annotations @Table And @Column

--- jokes-data/src/main/java/pl/pp/spring/jokeswebapp/model/User.java
+++ jokes-data/src/main/java/pl/pp/spring/jokeswebapp/model/User.java
@@ -1,12 +1,23 @@
 package pl.pp.spring.jokeswebapp.model;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
-//@Entity
+@Entity
+@Table(name = "user1")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
-    private String userName;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "password")
     private String password;
 
 }
