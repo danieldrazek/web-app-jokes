e03eecc81aabf421778bf6cf121a661f078d49c4
Daniel Drazek <daniel_2000@o2.pl>
2022-10-12 22:01:54 +0100

    annotation @Lob - CLOB, BLOB

---
+++ jokes-data/src/main/java/pl/pp/spring/jokeswebapp/model/UserProfile.java
@@ -0,0 +1,69 @@
+package pl.pp.spring.jokeswebapp.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class UserProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private LocalDate birthday;
+    @Lob
+    private String description;
+    @Lob
+    private Byte[] image;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(Byte[] image) {
+        this.image = image;
+    }
+}
