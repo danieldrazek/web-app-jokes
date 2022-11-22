e8a54437471d5aadfaa25f6dfe08895e9fea3f01
Daniel Drazek <daniel_2000@o2.pl>
2022-11-22 21:52:26 +0000

    Dedicated error page

--- jokes-web/src/test/java/pl/pp/spring/jokeswebapp/controllers/CategoryControllerTest.java
+++ jokes-web/src/test/java/pl/pp/spring/jokeswebapp/controllers/CategoryControllerTest.java
@@ -16,7 +16,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
