55c605dfb8a0cf73bdcba126d6be53d839c32d69
Daniel Drazek <daniel_2000@o2.pl>
2022-12-06 22:27:06 +0000

    category rest controller - get by id

--- jokes-web/src/main/java/pl/pp/spring/jokeswebapp/rest/CategoryRestController.java
+++ jokes-web/src/main/java/pl/pp/spring/jokeswebapp/rest/CategoryRestController.java
@@ -4,7 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import pl.pp.spring.jokeswebapp.exceptions.NotFoundException;
 import pl.pp.spring.jokeswebapp.model.Category;
 import pl.pp.spring.jokeswebapp.services.CategoryService;
 
@@ -21,11 +23,24 @@ public class CategoryRestController {
         this.categoryService = categoryService;
     }
 
-    @GetMapping({"api/categories"})
-    public ResponseEntity <List<Category>> showCategoriesList() {
+    @GetMapping("/api/categories")
+    public ResponseEntity<List<Category>> getAll() {
 
         log.info("showCategoriesList");
         return ResponseEntity.ok(categoryService.findAll());
+    }
+
+    @GetMapping("/api/categories/{id}")
+    public ResponseEntity<Category> getById(@PathVariable Long id) {
+
+        log.info("showCategoryById");
+
+        try {
+            Category category = categoryService.findById(id);
+            return ResponseEntity.ok(category);
+        } catch (NotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
